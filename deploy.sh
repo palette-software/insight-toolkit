10465f95c2aaf5e106863cc1fe59588124f4ea8b
Szilard Huber <szilard.huber@gmail.com>
2016-08-29 16:54:55 +0200

    I HATE Travis

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
   # Upload the RPM to the RPM repository
   # by exportin it to SSHPASS, sshpass wont log the command line and the password
-# export SSHPASS=$DEPLOY_PASS
+export SSHPASS=$DEPLOY_PASS
 sshpass -e scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r rpm-build/_build/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 
 # Update the RPM repository
