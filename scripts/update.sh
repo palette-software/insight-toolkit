03c25d81054ce936c321d60015c3f454e7208f78
Peter Bastyi <baspeti@gmail.com>
2016-08-31 09:00:04 +0200

    Pick a file descriptor number under 1024

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
+
+# Stop execution on the first error. Even if the error
+# is within the subshell (the block within parentheses).
 set -e
 
 (
-    # number chosen here is totally arbitrary. it is a file descriptor (FD)
-    # the only problem that can happen if someone else tries to flock on the same FD
-    # that case we would lock each other out. Hopefully termporarily.
-    # Note that the same FD needs to be used at the end of the subprocess definition (block)
-	flock -n 4263
+    # The number chosen here is totally arbitrary. It is a file descriptor (FD).
+    # The only problem that can happen if someone else tries to flock on the same FD.
+    # In that case we would lock each other out. Hopefully termporarily.
+    # Note that the same FD needs to be used at the end of the subprocess definition (block).
+    # And by default the ulimit -n (the maximum number of open file descriptors) value is
+    # 1024, so it's safer to pick a number under 1024.
+	flock -n 863
 
 	echo "$(date +"%Y-%m-%d %H:%M:%S") Update start"
 
@@ -24,4 +29,4 @@ set -e
 	/opt/insight-toolkit/update-loadtables.sh
 	echo "$(date +"%Y-%m-%d %H:%M:%S") Updated Palette Insight Load"
 
-) 4263>/tmp/insight-toolkit.flock
+) 863>/tmp/insight-toolkit.flock
