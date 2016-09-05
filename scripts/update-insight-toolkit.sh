185bdec850f90d0ad475f5fc3284570b7b0d6d06
Szilard Huber <szilard.huber@gmail.com>
2016-09-05 11:18:59 +0200

    Update toolkit and website as well.

---
+++ scripts/update-insight-toolkit.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Stop on the first error
+set -e
+
+# First try to update Palette Insight Server via yum, but if it fails,
+# try to perform an offline update.
+/opt/insight-toolkit/yum-or-offline-update.sh palette-insight-toolkit
+
+exit 0
