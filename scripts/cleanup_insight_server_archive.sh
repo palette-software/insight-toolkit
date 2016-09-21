230dc8a00ac331f3c2edb52e810a5264f37167ee
SELLER Peter Andras <spa@palette-software.com>
2016-09-22 00:30:40 +0200

    Traverse all archive subdirectories under /data/insight-server/uploads

--- scripts/cleanup_insight_server_archive.sh
+++ scripts/cleanup_insight_server_archive.sh
@@ -3,4 +3,13 @@
 # Clean up the .csv.gz files in the archive directory
 #
 
-"$(dirname "$0")"/cleanup_dir.sh /data/insight-server/uploads/*/archive/ 61 false
+INSIGHT_SERVER_DATA_DIR="/data/insight-server/uploads"
+
+CLUSTER_DIRS=$(find ${INSIGHT_SERVER_DATA_DIR} -type d -depth 1)
+
+for CLUSTER_DIR in ${CLUSTER_DIRS}; do
+    ARCHIVE_DIRS=$(find "${CLUSTER_DIR}/archive" -type d -depth 1)
+    for ARCHIVE_DIR in ${ARCHIVE_DIRS}; do
+        "$(dirname "$0")"/cleanup_dir.sh "${ARCHIVE_DIR}" 61 false
+    done
+done
