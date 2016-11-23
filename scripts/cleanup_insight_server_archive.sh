b45131bbf498728abecf7b7596f9d3964b3b6cef
Gergely Kovacs <kovacsg@starschema.net>
2016-11-23 15:34:22 +0100

    Add comments

--- scripts/cleanup_insight_server_archive.sh
+++ scripts/cleanup_insight_server_archive.sh
@@ -10,6 +10,7 @@ CLUSTER_DIRS=$(find ${INSIGHT_SERVER_DATA_DIR} -mindepth 1 -maxdepth 1 -type d)
 for CLUSTER_DIR in ${CLUSTER_DIRS}; do
     ARCHIVE_DIRS=$(find "${CLUSTER_DIR}/archive" -mindepth 1 -maxdepth 1 -type d)
     for ARCHIVE_DIR in ${ARCHIVE_DIRS}; do
+        # In order not to flood logs with deleted csv files the output is suppressed
         "$(dirname "$0")"/cleanup_dir.sh "${ARCHIVE_DIR}" 61 false > /dev/null
     done
 done
