867b0a96ba0c49fec9139abf074a42facb35166e
SELLER Peter Andras <spa@palette-software.com>
2017-01-11 12:37:48 +0100

    Add comment to 2.0 update script

--- scripts/2-0-upgrade.sh
+++ scripts/2-0-upgrade.sh
@@ -2,4 +2,6 @@
 
 sudo supervisorctl stop insight-gpfdist
 sudo supervisorctl stop palette-insight-server
+# Change the directory name from the name of the tenant to palette
+# It made rpm installation tenant independent
 mv /data/insight-server/uploads/`ls -l /data/insight-server/uploads/ | grep -v "_temp" | tr -s ' ' | cut -d' ' -f9 | tail -1` /data/insight-server/uploads/palette
